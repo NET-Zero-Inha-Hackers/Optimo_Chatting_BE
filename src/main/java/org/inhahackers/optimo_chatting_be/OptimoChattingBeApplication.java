91fce37e40937ff083faeda9096d7faaa14c8049
HEX <123macanic@naver.com>
2025-06-26 02:57:54 +0900

    feat: 프로젝트 환경설정 파일 추가

---
+++ src/main/java/org/inhahackers/optimo_chatting_be/OptimoChattingBeApplication.java
@@ -0,0 +1,13 @@
+package org.inhahackers.optimo_chatting_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimoChattingBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OptimoChattingBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/inhahackers/optimo_chatting_be/OptimoChattingBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.inhahackers.optimo_chatting_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimoChattingBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
